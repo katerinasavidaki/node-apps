2c44c7ee29783f40b80a0969b800a62d75f44424
Katerina Savidaki <ksavvidaki@gmail.com>
2025-04-08 19:25:04 +0300

    feat: logger with transports

--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -14,6 +14,7 @@ exports.findAll = async(req, res) => {
     res.status(200).json({status: true, data: result});
     logger.info("Success in reading all users");
     logger.warn("Success in reading all users");
+    logger.error()
   } catch (err) {
     console.log("Problem in reading users", err);
     logger.error("ERROR, Problem in reading all users", err);
